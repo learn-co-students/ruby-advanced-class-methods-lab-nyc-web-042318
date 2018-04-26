a97149668f1489a8192998145f9c8006f7471254
ensallee <ensallee@gmail.com>
2018-04-25 20:11:50 -0400

    solved

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @song = self.new
+    @@all << @song
+    @song
+  end
+
+  def self.new_by_name(song_name, artist_name=nil)
+    @song = self.new
+    @song.artist_name = artist_name
+    @song.name = song_name
+    @song
+  end
+
+  def self.create_by_name(song_name, artist_name=nil)
+    @song = self.create
+    @song.name = song_name
+    @song.artist_name = artist_name
+    @song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect do |song|
+      song.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      @song
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name}
+  end
+
+  def self.new_from_filename(song_file)
+    song_name_array=song_file.split(" - ")
+    self.new_by_name(song_name_array[1][0...-4])
+    @song.artist_name= song_name_array[0]
+    @song
+  end
+
+  def self.create_from_filename(song_file)
+    song_name_array=song_file.split(' - ')
+    self.create_by_name(song_name_array[1][0...-4])
+    @song.artist_name= song_name_array[0]
+    @song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
